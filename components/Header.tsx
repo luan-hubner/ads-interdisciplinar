e88ef2ad6ee36f024cbac442893ba50aa4d6e0cc
Luan Hubner <luanhubner.pro@gmail.com>
2023-01-10 09:42:21 -0400

    feat: update header link

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/components/Header.module.css'
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link to="informations" smooth={true} duration={600}>
+      <Link to="information" smooth={true} duration={600}>
         <a href="#">Informações</a>
       </Link>
       <Link to="projects" smooth={true} duration={600}>
